a8e9fcd02d3ad91ab6e8688baea863daff5b8a1b
Abhigna Gunda <abhignagunda1@gmail.com>
2025-11-01 03:22:06 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,6 @@ export const firebaseConfig = {
   "appId": "1:830224796229:web:1dd377992664f5eafe712c",
   "apiKey": "AIzaSyC5HmtsV7oFhCPdNEeXanoyHLf2AQaADeI",
   "authDomain": "portfolio-45a2c.firebaseapp.com",
-  "storageBucket": "portfolio-45a2c.appspot.com",
-  "messagingSenderId": "830224796229",
-  "measurementId": "G-4928J244X4"
+  "measurementId": "",
+  "messagingSenderId": "830224796229"
 };
