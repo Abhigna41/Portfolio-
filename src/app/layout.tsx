1f624899d570986eb2e598a95f585e4560919aec
Abhigna Gunda <abhignagunda1@gmail.com>
2025-11-01 04:10:38 +0000

    whatchanges did we make

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
-import { FirebaseClientProvider } from '@/firebase';
+import { FirebaseClientProvider } from '@/firebase/client-provider';
 import FloatingSocials from '@/components/floating-socials';
 import Footer from '@/components/footer';
 import Header from '@/components/header';
