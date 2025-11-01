0176778c00abd49bb13e4f53ffb5ff5081e24d7a
Abhigna Gunda <abhignagunda1@gmail.com>
2025-11-01 03:18:01 +0000

    Failed to load resource: the server responded with a status of 403 ()
    f

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:830224796229:web:1dd377992664f5eafe712c",
   "apiKey": "AIzaSyC5HmtsV7oFhCPdNEeXanoyHLf2AQaADeI",
   "authDomain": "portfolio-45a2c.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "830224796229"
+  "storageBucket": "portfolio-45a2c.appspot.com",
+  "messagingSenderId": "830224796229",
+  "measurementId": "G-4928J244X4"
 };
